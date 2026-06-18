d36a62b7b464dd87edd44abb6cc93d93fadd00a1
huyphat180-sketch <huyphat180@gmail.com>
2026-06-18 13:22:45 +0700

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,1 +1,26 @@
+const plants = [
+{
+id: 1,
+name: "Snake Plant",
+price: 15,
+category: "Air Purifying",
+image: "image-url"
+},
+{
+id: 2,
+name: "Peace Lily",
+price: 20,
+category: "Air Purifying",
+image: "image-url"
+}
+];
+<button
+onClick={() => dispatch(addItem(plant))}
+disabled={addedItems.includes(plant.id)}
+
+>
+
+{addedItems.includes(plant.id)
+? "Added"
+: "Add to Cart"} </button>
 
