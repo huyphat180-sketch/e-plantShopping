bc80b950e143a230ed7bcbb7a2d94b315660eb5e
huyphat180-sketch <huyphat180@gmail.com>
2026-06-18 13:19:41 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+
+function App() {
+return ( <div className="background-image"> <h1>Welcome to Paradise Nursery</h1>
+
+```
+  <p>
+    Discover beautiful indoor plants for your home and office.
+  </p>
+
+  <button>
+    Get Started
+  </button>
+</div>
+```
+
+);
+}
+
+export default App;
 
